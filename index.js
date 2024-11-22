ad2257679b08b848f1131214625f4bff1a2e31c4
xlaoru <sia212007@gmail.com>
2024-11-22 14:51:12 +0200

    env-uri-hidding

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://gym-sets-tracker.netlify.app",
+    origin: process.env.CORS_ORIGIN,
 }));
 
 const PORT = process.env.PORT || 3001;
