5e6bf1f9844c4d50bf7963fb2dce70737cc325ce
xlaoru <sia212007@gmail.com>
2025-02-08 19:53:34 +0200

    some-response-redesign

--- controllers/program-controller.js
+++ controllers/program-controller.js
@@ -3,10 +3,10 @@ const Program = require("../models/program.model");
 exports.getPrograms = async (req, res) => {
     try {
         const programs = await Program.find({});
-        res.status(200).json(programs);
+        res.status(200).json({ programs: programs, message: "Programs fetched successfully." });
     } catch (error) {
         console.log(error);
-        res.status(500).send("Error getting programs");
+        res.status(500).json({ message: "Error getting programs." });
     }
 }
 
@@ -14,30 +14,38 @@ exports.addProgram = async (req, res) => {
     try {
         const program = new Program(req.body);
         await program.save();
-        res.status(201).send("Program added");
+        res.status(201).json({ program, message: "Program created successfully." });
     } catch (error) {
         console.log(error);
-        res.status(500).send("Error adding program");
+        res.status(500).json({ message: "Error adding program." });
     }
 }
 
 exports.editProgram = async (req, res) => {
     try {
-        await Program.findByIdAndUpdate(req.params.id, req.body);
-        res.status(200).send("Program updated");
+        console.log("req.params.id", req.params.id);
+        console.log("req.body", req.body);
+
+
+        const program = await Program.findByIdAndUpdate(req.params.id, req.body);
+        await program.save()
+
+        res.status(200).json({ program, message: "Program edited successfully." });
     }
     catch (error) {
         console.log(error);
-        res.status(500).send("Error updating program");
+        res.status(500).json({ message: "Error updating program." });
     }
 }
 
 exports.deleteProgram = async (req, res) => {
     try {
-        await Program.findByIdAndDelete(req.params.id);
-        res.status(200).send("Program deleted");
+        const program = await Program.findByIdAndDelete(req.params.id);
+        await program.save()
+
+        res.status(200).json({ program, message: "Program deleted successfully." });
     } catch (error) {
         console.log(error);
-        res.status(500).send("Error deleting program");
+        res.status(500).json({ message: "Error deleting program." });
     }
 }
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
+    credentials: true
 }));
 
 const PORT = process.env.PORT || 3001;
